5df935962c8d526e9d00e6d7b5e9c75cd1cf4fe3
spd789562 <leo.yicun.lin@gmail.com>
2020-09-15 15:02:53 +0800

    [edit] result table and statistic move to component folder

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
 import React, { Fragment } from 'react'
-import dynamic from 'next/dynamic'
-import Head from 'next/head'
 import {
   Layout,
   Form,
-  Input,
-  Select,
   InputNumber,
   Row,
   Col,
-  Table,
   BackTop,
-  Statistic,
   Card,
   Avatar,
-  Space,
   Tooltip,
   Switch,
 } from 'antd'
+
+/* component */
 import ArcaneInputRangeSync from '../src/component/arcane-input-range-sync'
-// import { Line } from '@ant-design/charts'
+import ResultTable from '../src/component/result-table'
+import StatisticBoard from '../src/component/statistic-board'
 
 /* mapping */
 import ArcMapping from '../src/mapping/arcane'
@@ -36,224 +32,6 @@ import moment from 'moment'
 import styles from '../styles/Home.module.css'
 
 const { Header, Content, Footer } = Layout
-
-const Line = dynamic(() => import('@ant-design/charts/es/line'), {
-  ssr: false,
-})
-
-const renderIfMaxLevel = (text, row) =>
-  row.currentLevel === ArcInfo.maxLevel ||
-  row.currentLevel === 0 ||
-  row.dailyTotalCount === 0
-    ? {
-        children:
-          row.currentLevel === ArcInfo.maxLevel
-            ? '已滿級'
-            : row.currentLevel === 0
-            ? '未獲得此符文'
-            : `你每天都不拿，就卡在 ${row.currentLevel} 等吧`,
-        props: {
-          colSpan: 4,
-        },
-      }
-    : text
-const renderEmptyIfMaxLevel = (text, row) =>
-  row.currentLevel === ArcInfo.maxLevel ||
-  row.currentLevel === 0 ||
-  row.dailyTotalCount === 0
-    ? {
-        children: text,
-        props: {
-          colSpan: 0,
-        },
-      }
-    : typeof text === 'number'
-    ? numberFormat(text)
-    : text
-
-const ResultTable = ({ getFieldValue }) => {
-  const FinialData = ArcZone.map(({ name, key, daily, pquest }) => {
-    const {
-      count: currentCount,
-      daily: dailySymbol = 0,
-      quest: dailyQuest = 0,
-      party: dailyParty = 0,
-    } = getFieldValue(key)
-    const dailyTotalCount =
-      dailySymbol +
-      (dailyQuest ? daily : 0) +
-      (dailyParty && pquest
-        ? pquest.count ||
-          (dailyParty + (pquest.basic || 0)) / (pquest.unit || 1)
-        : 0)
-    const subTableData =
-      +!!dailyTotalCount !== 0 && +!!currentCount !== 0
-        ? ArcMapping.filter(({ stack }) => {
-            return currentCount < stack
-          }).map(({ level }) =>
-            parserTableData({
-              key: `${key}-${level}`,
-              level,
-              currentCount,
-              dailyTotalCount,
-            })
-          )
-        : []
-    return {
-      ...parserTableData({
-        key,
-        level: 20,
-        currentCount,
-        dailyTotalCount,
-      }),
-      dailyTotalCount,
-      currentCount,
-      name,
-      ...(subTableData.length ? { children: subTableData } : {}),
-    }
-  })
-  const today = moment().format('YYYY-MM-DD')
-
-  const chartData = Object.values(
-    FinialData.filter(({ currentLevel }) => currentLevel).reduce(
-      (acc, inc) => {
-        acc.data.push({
-          date: today,
-          type: inc.key,
-          value: (inc.currentLevel + 2) * 10,
-        })
-        acc.total[today] = (acc.total[today] || 0) + (inc.currentLevel + 2) * 10
-        if (inc.children) {
-          inc.children.forEach(({ completeDate }) => {
-            acc.data.push({
-              date: completeDate,
-              type: inc.key,
-              value: 10,
-            })
-            acc.total[completeDate] = (acc.total[completeDate] || 0) + 10
-          })
-        }
-        return acc
-      },
-      { data: [], total: {} }
-    )
-  )
-    .reduce(
-      (acc, inc) =>
-        Array.isArray(inc)
-          ? [...inc, ...acc]
-          : [
-              ...Object.entries(inc).map(([date, value]) => ({
-                date,
-                type: '總和',
-                value,
-              })),
-              ...acc,
-            ],
-      []
-    )
-    .sort((a, b) => moment(a.date).unix() - moment(b.date).unix())
-    .reduce(
-      (acc, inc) => {
-        if (!acc.data[inc.date]) acc.data[inc.date] = { ...acc.stack }
-        acc.stack[inc.type] = (acc.stack[inc.type] || 0) + inc.value
-        acc.data[inc.date][inc.type] = acc.stack[inc.type]
-        return acc
-      },
-      { data: {}, stack: {} }
-    ).data
-  const testData = Object.entries(chartData)
-    .map(([date, data]) =>
-      Object.entries(data).map(([type, value]) => ({
-        type,
-        value,
-        date,
-      }))
-    )
-    .reduce((acc, inc) => acc.concat(inc), [])
-  // .reduce((acc, inc, index, arr) => {
-  //   if (!acc.data) {
-  //     acc.data = arr
-  //     acc.stack = {}
-  //   }
-  //   if (!acc.stack[inc.type]) {
-  //     acc.stack[inc.type] = inc.value
-  //   } else {
-  //     acc.stack[inc.type] += inc.value
-  //     acc.data[index].value = acc.stack[inc.type]
-  //   }
-  //   return acc
-  // }, {})
-  return (
-    <Fragment>
-      <Table
-        columns={[
-          {
-            title: '地區',
-            dataIndex: 'name',
-            key: 'name',
-            width: 120,
-            align: 'center',
-          },
-          {
-            title: '等級',
-            dataIndex: 'level',
-            key: 'level',
-            align: 'center',
-            width: 60,
-            render: renderIfMaxLevel,
-          },
-          {
-            title: '達成日期(天數)',
-            dataIndex: 'completeDateText',
-            key: 'completeDateText',
-            align: 'center',
-            width: 190,
-            render: renderEmptyIfMaxLevel,
-          },
-          {
-            title: '累計符文',
-            dataIndex: 'accumulativeNeed',
-            key: 'accumulativeNeed',
-            align: 'center',
-            width: 100,
-            render: renderEmptyIfMaxLevel,
-          },
-          {
-            title: '累計楓幣',
-            dataIndex: 'totalCost',
-            key: 'totalCost',
-            align: 'center',
-            width: 120,
-            render: renderEmptyIfMaxLevel,
-          },
-        ]}
-        dataSource={FinialData}
-        pagination={false}
-        scroll={{ x: '100%' }}
-        sticky
-      ></Table>
-      {/* <Line
-        {...{
-          title: {
-            visible: true,
-            text: 'arc 趨勢圖',
-          },
-          forceFit: true,
-          data: testData,
-          padding: 'auto',
-          xField: 'date',
-          yField: 'value',
-          seriesField: 'type',
-          xAxis: {
-            type: 'dateTime',
-          },
-          legend: { position: 'right-top' },
-        }}
-      ></Line> */}
-    </Fragment>
-  )
-}
 
 export default function Home() {
   const [form] = Form.useForm()
@@ -401,141 +179,27 @@ export default function Home() {
               </Col>
             ))}
           </Row>
-          <Form.Item
-            shouldUpdate={() => true}
-            wrapperCol={{ xs: 24, sm: 24 }}
-            style={{ marginBottom: 0 }}
-          >
-            {({ getFieldValue }) => {
-              const statisticData = ArcZone.map(
-                ({ name, key, daily, pquest }) => {
-                  const {
-                    count: currentCount,
-                    daily: dailySymbol = 0,
-                    quest: dailyQuest,
-                    party: dailyParty = 0,
-                  } = getFieldValue(key)
-                  const dailyTotalCount =
-                    dailySymbol +
-                    (dailyQuest ? daily : 0) +
-                    (dailyParty && pquest
-                      ? pquest.count ||
-                        (dailyParty + (pquest.basic || 0)) / (pquest.unit || 1)
-                      : 0)
-                  const { completeDate, remainDays } = parserTableData({
-                    key,
-                    level: 20,
-                    currentCount,
-                    dailyTotalCount,
-                  })
-                  return {
-                    level: symbolMatch(currentCount).level || 0,
-                    completeDate,
-                    remainDays,
+          <Form.Item shouldUpdate wrapperCol={{ xs: 24, sm: 24 }}>
+            {({ getFieldsValue }) => {
+              const fieldsValue = getFieldsValue()
+              const data = Object.keys(fieldsValue).length
+                ? fieldsValue
+                : {
+                    vanishingjourney: {},
+                    chuchu: {},
+                    lachelein: {},
+                    arcana: {},
+                    morass: {},
+                    esfera: {},
+                    role: 0,
                   }
-                }
-              ).reduce(
-                (acc, inc) => {
-                  acc.completeDate = acc.completeDate
-                    ? moment(inc.completeDate).isValid() &&
-                      moment(inc.completeDate).isAfter(acc.completeDate, 'days')
-                      ? inc.completeDate
-                      : acc.completeDate
-                    : moment(inc.completeDate).isValid()
-                    ? inc.completeDate
-                    : undefined
-                  acc.total += inc.level
-                  acc.holded += inc.level !== 0 ? 1 : 0
-                  acc.remainDays =
-                    inc.remainDays !== Infinity &&
-                    inc.remainDays > acc.remainDays
-                      ? inc.remainDays
-                      : acc.remainDays
-                  return acc
-                },
-                { total: 0, holded: 0, remainDays: 0 }
-              )
               return (
-                <Row gutter={[8, 8]}>
-                  <Col xs={24} sm={12} md={6}>
-                    <Card>
-                      <Statistic
-                        title="Arc"
-                        value={numberFormat(
-                          statisticData.total * 10 + statisticData.holded * 20
-                        )}
-                        suffix={`/ ${numberFormat(statisticData.holded * 220)}`}
-                      />
-                    </Card>
-                  </Col>
-                  <Col xs={24} sm={12} md={6}>
-                    <Card>
-                      <Row>
-                        <Col span={12}>
-                          <Form.Item shouldUpdate={() => true} noStyle>
-                            {({ getFieldValue }) => {
-                              const role = getFieldValue('role')
-                              return (
-                                <Statistic
-                                  title="屬性加成量"
-                                  value={numberFormat(
-                                    (statisticData.total +
-                                      statisticData.holded * 2) *
-                                      RoleMapping[role].unit
-                                  )}
-                                />
-                              )
-                            }}
-                          </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                          <Form.Item name="role" noStyle>
-                            <Select>
-                              {RoleMapping.map(({ name }, index) => (
-                                <Select.Option
-                                  value={index}
-                                  key={`role-${index}`}
-                                >
-                                  {name}
-                                </Select.Option>
-                              ))}
-                            </Select>
-                          </Form.Item>
-                        </Col>
-                      </Row>
-                    </Card>
-                  </Col>
-                  <Col xs={24} sm={24} md={12}>
-                    <Card>
-                      <Statistic
-                        title="完成日期(天數)"
-                        value={
-                          statisticData.total
-                            ? statisticData.remainDays === 0
-                              ? statisticData.total * 10 +
-                                  statisticData.holded * 20 ===
-                                statisticData.holded * 220
-                                ? '已達成當前上限'
-                                : '永遠完成不了'
-                              : statisticData.completeDate
-                            : '無'
-                        }
-                        suffix={
-                          statisticData.remainDays
-                            ? `(${numberFormat(statisticData.remainDays)}天)`
-                            : ''
-                        }
-                      />
-                    </Card>
-                  </Col>
-                </Row>
+                <Fragment>
+                  <StatisticBoard data={data} />
+                  <ResultTable data={data} />
+                </Fragment>
               )
             }}
-          </Form.Item>
-          <Form.Item shouldUpdate={() => true} wrapperCol={{ xs: 24, sm: 24 }}>
-            {({ getFieldValue }) => (
-              <ResultTable getFieldValue={getFieldValue} />
-            )}
           </Form.Item>
         </Form>
       </Content>
---
+++ src/component/result-table.jsx
@@ -0,0 +1,247 @@
+import dynamic from 'next/dynamic'
+
+import { Fragment } from 'react'
+import { Table } from 'antd'
+
+/* mapping */
+import ArcaneSymbolMapping from '../mapping/arcane'
+import ArcaneSymbol from '../mapping/arcane-info'
+import ArcZone from '../mapping/arcane-river-zone'
+
+/* utils */
+import numberFormat from '../util/number-format'
+import parserTableData from '../util/parser-table-data'
+import moment from 'moment'
+
+const Line = dynamic(() => import('@ant-design/charts/es/line'), {
+  ssr: false,
+})
+
+const renderIfMaxLevel = (text, row) =>
+  row.currentLevel === ArcaneSymbol.maxLevel ||
+  row.currentLevel === 0 ||
+  row.dailyTotalCount === 0
+    ? {
+        children:
+          row.currentLevel === ArcaneSymbol.maxLevel
+            ? '已滿級'
+            : row.currentLevel === 0
+            ? '未獲得此符文'
+            : `你每天都不拿，就卡在 ${row.currentLevel} 等吧`,
+        props: {
+          colSpan: 4,
+        },
+      }
+    : text
+
+const renderEmptyIfMaxLevel = (text, row) =>
+  row.currentLevel === ArcaneSymbol.maxLevel ||
+  row.currentLevel === 0 ||
+  row.dailyTotalCount === 0
+    ? {
+        children: text,
+        props: {
+          colSpan: 0,
+        },
+      }
+    : typeof text === 'number'
+    ? numberFormat(text)
+    : text
+
+const useTableData = (data) =>
+  ArcZone.map(({ name, key, daily, pquest }) => {
+    const {
+      count: currentCount,
+      daily: dailySymbol = 0,
+      quest: dailyQuest = 0,
+      party: dailyParty = 0,
+    } = data[key]
+    const dailyQuestCount = dailyQuest ? daily : 0
+    // has party quest
+    const dailyPartyQuestCount =
+      dailyParty && pquest
+        ? pquest.count ||
+          // if not a static value then calculating
+          (dailyParty + (pquest.basic || 0)) / (pquest.unit || 1)
+        : 0
+    const dailyTotalCount = dailySymbol + dailyQuestCount + dailyPartyQuestCount
+    const subTableData =
+      !!dailyTotalCount && !!currentCount
+        ? ArcaneSymbolMapping.filter(({ stack }) => {
+            return currentCount < stack
+          }).map(({ level }) =>
+            parserTableData({
+              key: `${key}-${level}`,
+              level,
+              currentCount,
+              dailyTotalCount,
+            })
+          )
+        : []
+    return {
+      ...parserTableData({
+        key,
+        level: 20,
+        currentCount,
+        dailyTotalCount,
+      }),
+      dailyTotalCount,
+      currentCount,
+      name,
+      ...(subTableData.length ? { children: subTableData } : {}),
+    }
+  })
+
+const useChartData = (tableData) => {
+  const today = moment().format('YYYY-MM-DD')
+  const chartData = Object.values(
+    tableData
+      .filter(({ currentLevel }) => currentLevel)
+      .reduce(
+        (acc, inc) => {
+          acc.data.push({
+            date: today,
+            type: inc.key,
+            value: (inc.currentLevel + 2) * 10,
+          })
+          acc.total[today] =
+            (acc.total[today] || 0) + (inc.currentLevel + 2) * 10
+          if (inc.children) {
+            inc.children.forEach(({ completeDate }) => {
+              acc.data.push({
+                date: completeDate,
+                type: inc.key,
+                value: 10,
+              })
+              acc.total[completeDate] = (acc.total[completeDate] || 0) + 10
+            })
+          }
+          return acc
+        },
+        { data: [], total: {} }
+      )
+  )
+    .reduce(
+      (acc, inc) =>
+        Array.isArray(inc)
+          ? [...inc, ...acc]
+          : [
+              ...Object.entries(inc).map(([date, value]) => ({
+                date,
+                type: '總和',
+                value,
+              })),
+              ...acc,
+            ],
+      []
+    )
+    .sort((a, b) => moment(a.date).unix() - moment(b.date).unix())
+    .reduce(
+      (acc, inc) => {
+        if (!acc.data[inc.date]) acc.data[inc.date] = { ...acc.stack }
+        acc.stack[inc.type] = (acc.stack[inc.type] || 0) + inc.value
+        acc.data[inc.date][inc.type] = acc.stack[inc.type]
+        return acc
+      },
+      { data: {}, stack: {} }
+    ).data
+
+  return Object.entries(chartData)
+    .map(([date, data]) =>
+      Object.entries(data).map(([type, value]) => ({
+        type,
+        value,
+        date,
+      }))
+    )
+    .reduce((acc, inc) => acc.concat(inc), [])
+
+  // .reduce((acc, inc, index, arr) => {
+  //   if (!acc.data) {
+  //     acc.data = arr
+  //     acc.stack = {}
+  //   }
+  //   if (!acc.stack[inc.type]) {
+  //     acc.stack[inc.type] = inc.value
+  //   } else {
+  //     acc.stack[inc.type] += inc.value
+  //     acc.data[index].value = acc.stack[inc.type]
+  //   }
+  //   return acc
+  // }, {})
+}
+
+const ResultTable = ({ data }) => {
+  const tableData = useTableData(data)
+  return (
+    <Fragment>
+      <Table
+        columns={[
+          {
+            title: '地區',
+            dataIndex: 'name',
+            key: 'name',
+            width: 120,
+            align: 'center',
+          },
+          {
+            title: '等級',
+            dataIndex: 'level',
+            key: 'level',
+            align: 'center',
+            width: 60,
+            render: renderIfMaxLevel,
+          },
+          {
+            title: '達成日期(天數)',
+            dataIndex: 'completeDateText',
+            key: 'completeDateText',
+            align: 'center',
+            width: 190,
+            render: renderEmptyIfMaxLevel,
+          },
+          {
+            title: '累計符文',
+            dataIndex: 'accumulativeNeed',
+            key: 'accumulativeNeed',
+            align: 'center',
+            width: 100,
+            render: renderEmptyIfMaxLevel,
+          },
+          {
+            title: '累計楓幣',
+            dataIndex: 'totalCost',
+            key: 'totalCost',
+            align: 'center',
+            width: 120,
+            render: renderEmptyIfMaxLevel,
+          },
+        ]}
+        dataSource={tableData}
+        pagination={false}
+        scroll={{ x: '100%' }}
+        sticky
+      ></Table>
+      {/* <Line
+        {...{
+          title: {
+            visible: true,
+            text: 'arc 趨勢圖',
+          },
+          forceFit: true,
+          data: testData,
+          padding: 'auto',
+          xField: 'date',
+          yField: 'value',
+          seriesField: 'type',
+          xAxis: {
+            type: 'dateTime',
+          },
+          legend: { position: 'right-top' },
+        }}
+      ></Line> */}
+    </Fragment>
+  )
+}
+
+export default ResultTable
