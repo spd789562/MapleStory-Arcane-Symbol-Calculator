96cf17ce30de856de98a09914df086ef5b8b8db7
spd789562 <leo.yicun.lin@gmail.com>
2020-09-17 14:57:12 +0800

    [edit] mapping file data change to i18n format

--- src/mapping/arcane-river-zone.js
+++ src/mapping/arcane-river-zone.js
@@ -18,45 +18,45 @@
  */
 const ArcaneRiverZone = [
   {
-    name: '消逝的旅途',
+    name: 'vanishing_journey',
     daily: 8,
-    pquest: { name: '艾爾達斯光譜', type: 'symbol', count: 6 },
+    pquest: { name: 'edra_spectrum', type: 'symbol', count: 6 },
     key: 'vanishingjourney',
     levelupDiscount: true,
   },
   {
-    name: '啾啾愛爾蘭',
+    name: 'chu_chu_island',
     daily: 4,
-    pquest: { name: '肚子餓的武藤', type: 'symbol', dailyMax: 15 },
+    pquest: { name: 'hungry_muto', type: 'symbol', dailyMax: 15 },
     key: 'chuchu',
   },
   {
-    name: '拉契爾恩',
+    name: 'lachelein',
     key: 'lachelein',
     daily: 4,
     pquest: {
-      name: '毀夢者',
+      name: 'dream_defender',
       type: 'coin',
       unit: 30,
       basic: 24,
       dailyMax: 500,
-      desc: `毀夢者: 最多可獲得 ${500} 個, ${30} 個可兌換 1 個符文, 已自動計入雕像對話保底 24 個`,
+      desc: 'party_quest_tips_exchange_basic',
     },
   },
   {
-    name: '阿爾卡納',
+    name: 'arcana',
     key: 'arcana',
     daily: 8,
     pquest: {
-      name: '精靈救援者',
+      name: 'spirit_savior',
       type: 'coin',
       unit: 3,
       basic: 0,
       dailyMax: 30,
     },
   },
-  { name: '魔菈斯', daily: 8, key: 'morass' },
-  { name: '艾斯佩拉', daily: 8, key: 'esfera' },
+  { name: 'morass', daily: 8, key: 'morass' },
+  { name: 'esfera', daily: 8, key: 'esfera' },
 ]
 
 export default ArcaneRiverZone
