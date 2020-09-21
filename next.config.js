3268f5485b51d07f4fc1416ad00b78f421bb8c6a
spd789562 <leo.yicun.lin@gmail.com>
2020-09-21 17:34:51 +0800

    [add] service worker init

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+const { nextI18NextRewrites } = require('next-i18next/rewrites')
 const withSass = require('@zeit/next-sass')
 const withLess = require('@zeit/next-less')
 const withCSS = require('@zeit/next-css')
+const withPWA = require('next-pwa')
+const path = require('path')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const localeSubpaths = {
+  en: 'en',
+  zh_tw: 'zh_tw',
+  zh_cn: 'zh_cn',
+}
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
   require.extensions['.less'] = (file) => {}
 }
 
-module.exports = {
+module.exports = withPWA({
   ...withCSS({
     cssModules: true,
     cssLoaderOptions: {
@@ -19,14 +30,28 @@ module.exports = {
         lessLoaderOptions: {
           javascriptEnabled: true,
         },
+        webpack(config, options) {
+          if (!options.isServer && config.mode === 'development') {
+            const { I18NextHMRPlugin } = require('i18next-hmr/plugin')
+            config.plugins.push(
+              new I18NextHMRPlugin({
+                localesDir: path.resolve(__dirname, 'public/static/locales'),
+              })
+            )
+          }
+
+          return config
+        },
       })
     ),
   }),
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     GOOGlE_AD_ID: process.env.GOOGlE_AD_ID || '',
     GOOGlE_AD_SLOT: process.env.GOOGlE_AD_SLOT || '',
     GOOGlE_AD_SLOT_TEST: process.env.GOOGlE_AD_SLOT_TEST || '',
     GOOGLE_AD_LAYOUT_TEST: process.env.GOOGLE_AD_LAYOUT_TEST || '',
     GOOGlE_ANALYTICS_ID: process.env.GOOGlE_ANALYTICS_ID || '',
+    localeSubpaths,
   },
-}
+})
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,23 @@
 const NextI18Next = require('next-i18next').default
+const { localeSubpaths } = require('next/config').default().publicRuntimeConfig
 const path = require('path')
 
-module.exports = new NextI18Next({
+const nextI18Next = new NextI18Next({
   defaultNS: 'index',
   defaultLanguage: 'zh_tw',
   otherLanguages: ['en', 'zh_cn'],
-  localeSubpaths: {
-    en: 'en',
-    zh_tw: 'zh_tw',
-    zh_cn: 'zh_cn',
-  },
+  localeSubpaths,
   localePath: path.resolve('./public/static/locales'),
 })
+
+if (process.env.NODE_ENV !== 'production') {
+  if (process.browser) {
+    const { applyClientHMR } = require('i18next-hmr/client')
+    applyClientHMR(nextI18Next.i18n)
+  } else {
+    const { applyServerHMR } = require('i18next-hmr/server')
+    applyServerHMR(nextI18Next.i18n)
+  }
+}
+
+module.exports = nextI18Next
