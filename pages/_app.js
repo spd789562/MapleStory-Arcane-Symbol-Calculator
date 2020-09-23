5e2421ca7fbc222eed5259042029f8e7a37eec2a
Lin Yi-Cun <Leo.yicun.Lin@gmail.com>
2020-09-23 14:28:38 +0800

    seo locale (#9)
    * [edit] reset locale
    
    * [add] description locale

--- pages/_app.js
+++ pages/_app.js
@@ -12,23 +12,20 @@ const NextHead = withTranslation('index')(({ t, i18n: { language } }) => (
       name="viewport"
       content="width=device-width, initial-scale=1, maximum-scale=1"
     />
-    <meta name="description" content="計算楓之谷秘法符文完成日期" />
+    <meta name="description" content={t('seo_desc')} />
     <meta
       name="keywords"
       content="秘法符文計算機,arc計算機,arc,秘法符文,Arcane Symbol Calculator,Arcane Symbol,Arcane SymbolPower"
     />
     <meta property="og:type" content="website" />
-    <meta
-      property="og:description"
-      content="計算楓之谷秘法符文完成日期及所需楓幣"
-    />
+    <meta property="og:description" content={t('seo_desc')} />
     <meta
       property="og:url"
       content="https://maplestory-arcane-symbol-calculator.vercel.app/"
     />
-    <meta property="og:locale" content="zh_tw" />
-    <meta property="og:site_name" content="秘法符文計算機" />
-    <meta property="og:title" content="秘法符文計算機" />
+    <meta property="og:locale" content={language} />
+    <meta property="og:site_name" content={t('title')} />
+    <meta property="og:title" content={t('title')} />
     <link
       href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap"
       rel="stylesheet"
