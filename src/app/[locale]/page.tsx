be61b89eeb794b5d097595cb83429f4d69e64afa
spd789562 <leo.yicun.lin@gmail.com>
2023-11-06 18:46:38 +0800

    [add] odd links component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
 import Layout from 'antd/lib/layout';
 
-import { Footer, Header } from 'antd/lib/layout/layout';
+import { Footer, Header, Content } from 'antd/lib/layout/layout';
+import BackTop from 'antd/lib/float-button/BackTop';
 
 import PageHeader from '@/components/Headers/PageHeader';
+import OldVersionLinks from '@/components/OldVersionLinks';
 import PageFooter from '@/components/PageFooter';
 
 export default function Home() {
@@ -11,6 +13,10 @@ export default function Home() {
       <Header className="bg-header-bg p-0 h-unset">
         <PageHeader />
       </Header>
+      <Content className="pt-2 px-4 xl:px-0 w-full xl:max-w-header-max xl:mx-auto">
+        <OldVersionLinks />
+      </Content>
+      <BackTop />
       <Footer className="text-center px-4">
         <PageFooter />
       </Footer>
