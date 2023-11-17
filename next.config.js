f7e34b069a1ed52c8d9df8853c12914d3422f88b
spd789562 <leo.yicun.lin@gmail.com>
2023-11-17 10:48:34 +0800

    [add] migrate old lang path

--- next.config.js
+++ next.config.js
@@ -2,11 +2,21 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
-})
+});
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  /* conver old i18n path to new: zh_tw -> zh-tw */
+  redirects: async () => {
+    return [
+      {
+        source: '/:locale(zh_tw|zh_cn)',
+        destination: '/:locale(zh-tw|zh-cn)',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = withPWA(nextConfig)
+module.exports = withPWA(nextConfig);
